669087267fc17dee05c615a4f197bdeae2c010ff
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 20:21:42 -0300

    Create Container_AddToExistingStackable.cs

---
+++ VisualStudio/Patches/Container_AddToExistingStackable.cs
@@ -0,0 +1,17 @@
+﻿namespace StackManager.Patches
+{
+	/* Logic:
+		UNKOWN
+	*/
+	[HarmonyPatch(typeof(Container))]
+	[HarmonyPatch(nameof(Container.AddToExistingStackable))]
+	[HarmonyPatch([typeof(GearItem), typeof(float), typeof(int)])]
+	public class Container_AddToExistingStackable
+	{
+		public static bool Prefix(ref GearItem gearToAdd, float normalizedCondition, int numUnits)
+		{
+			if (gearToAdd.gameObject.GetComponent<StackableItem>() != null) return true;
+			return StackingUtilities.Do(gearToAdd, normalizedCondition, numUnits);
+		}
+	}
+}
