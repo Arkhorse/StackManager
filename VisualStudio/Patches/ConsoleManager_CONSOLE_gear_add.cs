676a2bf3fb3d945d739264f6feea0f1e9addf856
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 20:21:47 -0300

    Update ConsoleManager_CONSOLE_gear_add.cs

--- VisualStudio/Patches/ConsoleManager_CONSOLE_gear_add.cs
+++ VisualStudio/Patches/ConsoleManager_CONSOLE_gear_add.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StackManager.Patches
+﻿namespace StackManager.Patches
 {
+    /*
     [HarmonyPatch(typeof(ConsoleManager), nameof(ConsoleManager.CONSOLE_gear_add))]
-    internal class ConsoleManager_CONSOLE_gear_add
+    public class ConsoleManager_CONSOLE_gear_add
     {
-
-        private static void Postfix()
+        public static void Postfix()
         {
             // are we tracking a postfix patch ?
-            if (Main.PostFixTrack)
+            if (Main.PostfixTrack.PostFixTrack)
             {
-                if (Main.PostfixStack != null)
+                if (Main.PostfixTrack.PostfixStack != null)
                 {
                     // correct the stack(s) conditions
-                    Main.PostfixStack.CurrentHP = Main.PostfixCondition * Main.PostfixStack.m_GearItemData.m_MaxHP;
+                    Main.PostfixTrack.PostfixStack.CurrentHP = Main.PostfixTrack.PostfixCondition * Main.PostfixTrack.PostfixStack.m_GearItemData.m_MaxHP;
                     // reset the items constraints
-                    Main.PostfixStack.m_StackableItem.m_StackConditionDifferenceConstraint = Main.PostfixConstraint;
+                    Main.PostfixTrack.PostfixStack.m_StackableItem.m_StackConditionDifferenceConstraint = Main.PostfixTrack.PostfixConstraint;
                 }
-                if (Main.PostfixGearToAdd != null)
+                if (Main.PostfixTrack.PostfixGearToAdd != null)
                 {
                     // correct the stack(s) conditions
-                    Main.PostfixGearToAdd.CurrentHP = Main.PostfixCondition * Main.PostfixGearToAdd.m_GearItemData.m_MaxHP;
+                    Main.PostfixTrack.PostfixGearToAdd.CurrentHP = Main.PostfixTrack.PostfixCondition * Main.PostfixTrack.PostfixGearToAdd.m_GearItemData.m_MaxHP;
                     // reset the items constraints
-                    Main.PostfixGearToAdd.m_StackableItem.m_StackConditionDifferenceConstraint = Main.PostfixConstraint;
+                    Main.PostfixTrack.PostfixGearToAdd.m_StackableItem.m_StackConditionDifferenceConstraint = Main.PostfixTrack.PostfixConstraint;
                 }
             }
 
             // reset the static values to avoid any conflicts
-            StackingUtilities.ResetPostfixParams();
+            StackingUtilities.SetPostFix(null, null, 0, 0, true);
         }
-
     }
+    */
 }
