ab28d3295f12b2472f6589251bb9fc9175b63cd5
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 20:21:38 -0300

    Create PlayerManager_TryAddToExistingStackable_Reflection.cs

---
+++ VisualStudio/Patches/PlayerManager_TryAddToExistingStackable_Reflection.cs
@@ -0,0 +1,29 @@
+﻿namespace StackManager.Patches
+{
+	/*
+	[HarmonyPatch]
+	public class PlayerManager_TryAddToExistingStackable_Reflection
+	{
+		public static MethodBase? TargetMethod()
+		{
+			MethodInfo? targetMethod = typeof(PlayerManager).GetMethods().FirstOrDefault(m => m.Name == nameof(PlayerManager.TryAddToExistingStackable) && m.ReturnType == typeof(bool) && !m.IsGenericMethod && m.GetParameters().Length == 4);
+
+			if (targetMethod is null)
+			{
+				throw new BadMemeException("PlayerManager.TryAddToExistingStackable(GearItem gearToAdd, float normalizedCondition, int numUnits, ref GearItem? existingGearItem) not found for patch.");
+			}
+			else if (targetMethod != null)
+			{
+				Logging.Log($"targetMethod::Found");
+			}
+
+			return targetMethod;
+		}
+
+		public static void Prefix()
+		{
+			Logging.Log($"PlayerManager_TryAddToExistingStackable_Reflection::Found");
+		}
+	}
+	*/
+}
