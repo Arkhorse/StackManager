f87ed12d002bb5850fa08776006500bf7f71517b
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 20:21:54 -0300

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -8,6 +8,10 @@ namespace StackManager
         [Description("If enabled, the mod will add stackable behaviour to items defined in the config (this is not something you can add to)")]
         public bool AddStack = true;
 
+        [Name("Set to max condition")]
+        [Description("If enabled, will always use the max condition of items when stacking. DOES NOT AFFECT ITEMS NOT TOUCHED BY THIS MOD")]
+        public bool UseMaxHP = false;
+
         // This is used to load the settings
         internal static void OnLoad()
         {
