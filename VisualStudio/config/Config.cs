2534f2c4d188d080124c87b6a30c859cc022a547
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-21 18:30:53 -0400

    Update Config.cs

--- VisualStudio/config/Config.cs
+++ VisualStudio/config/Config.cs
@@ -2,15 +2,15 @@
 
 namespace StackManager.config
 {
-    public class Config
-    {
-        [JsonInclude]
-        public Version ConfigurationVersion { get; set; } = new Version();
-        [JsonInclude]
-        public List<string> STACK_MERGE { get; set; } = new();
-        [JsonInclude]
-        public List<string> Advanced { get; set; } = new();
-        [JsonInclude]
-        public List<string> AddStackableComponent { get; set; } = new();
-    }
+	public class Config
+	{
+		[JsonInclude]
+		public Version ConfigurationVersion { get; set; } = new();
+		[JsonInclude]
+		public List<string> STACK_MERGE { get; set; } = new();
+		[JsonInclude]
+		public List<string> Advanced { get; set; } = new();
+		[JsonInclude]
+		public List<string> AddStackableComponent { get; set; } = new();
+	}
 }
