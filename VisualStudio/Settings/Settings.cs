8c72fa38118d405811dbe44ef922b99b55ea878e
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-21 18:30:45 -0400

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -1,22 +1,22 @@
 namespace StackManager
 {
-    public class Settings : JsonModSettings
-    {
-        internal static Settings Instance { get; } = new();
+	public class Settings : JsonModSettings
+	{
+		internal static Settings Instance { get; } = new();
 
-        [Name("Add Stackable Behaviour")]
-        [Description("If enabled, the mod will add stackable behaviour to items defined in the config (this is not something you can add to)")]
-        public bool AddStack = true;
+		[Name("Add Stackable Behaviour")]
+		[Description("If enabled, the mod will add stackable behaviour to items defined in the config (this is not something you can add to)")]
+		public bool AddStack = true;
 
-        [Name("Set to max condition")]
-        [Description("If enabled, will always use the max condition of items when stacking. DOES NOT AFFECT ITEMS NOT TOUCHED BY THIS MOD")]
-        public bool UseMaxHP = false;
+		[Name("Set to max condition")]
+		[Description("If enabled, will always use the max condition of items when stacking. DOES NOT AFFECT ITEMS NOT TOUCHED BY THIS MOD")]
+		public bool UseMaxHP = false;
 
-        // This is used to load the settings
-        internal static void OnLoad()
-        {
-            Instance.AddToModSettings(BuildInfo.GUIName);
-            Instance.RefreshGUI();
-        }
-    }
+		// This is used to load the settings
+		internal static void OnLoad()
+		{
+			Instance.AddToModSettings(BuildInfo.GUIName);
+			Instance.RefreshGUI();
+		}
+	}
 }
